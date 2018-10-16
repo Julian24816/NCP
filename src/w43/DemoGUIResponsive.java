206a8fae0ff675e60bcd01b5ad0c621b6da9f411
Julian24816 <Julian24816@gmail.com>
2018-10-16 17:28:23 +0200

    task 4 - making GUI responsive by putting long task into separate thread

---
+++ src/w43/DemoGUIResponsive.java
@@ -0,0 +1,50 @@
+package w43;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DemoGUIResponsive extends JFrame {
+
+    private static class ButtonDemoPanel extends JPanel {
+        private JButton start, stop;
+
+        private ButtonDemoPanel() {
+            start = new JButton("Start long task");
+            stop = new JButton("Test if still responsive ");
+            add(start);
+            add(stop);
+            stop.addActionListener(e -> System.out.println("CLICK"));
+            start.addActionListener(e -> new Thread(() -> {
+                try {
+                    System.out.println("Start long task");
+                    Thread.sleep(5000);
+                    System.out.println("End long task");
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }).start());
+        }
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(DemoGUIResponsive::createAndShowGUI);
+    }
+
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("(Un-)responsive GUI Demo");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JLabel emptyLabel = new JLabel("");
+        emptyLabel.setPreferredSize(new Dimension(400, 100));
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+
+        frame.getContentPane().add(new ButtonDemoPanel());
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
+
