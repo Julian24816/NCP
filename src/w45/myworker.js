a260dfe70be587f501566ce0e13edd96c6581e03
Julian24816 <Julian24816@gmail.com>
2018-10-30 17:29:34 +0100

    javascript json method passing for gentle termination

---
+++ src/w45/myworker.js
@@ -0,0 +1,26 @@
+var i = 0;
+var isTerminated = false;
+
+function timedCount() {
+    if (!isTerminated) {
+        setTimeout("timedCount()", 500)
+        i = i + 1;
+        console.log("[worker] posting :" + i);
+        postMessage({"type":"data", "content":i});
+    } else self.close();
+}
+
+self.onmessage = function (e) {
+    console.log("[worker] message received: " + JSON.stringify(e.data));
+    switch (e.data.cmd) {
+        case 'stop':
+            console.log("[worker] self-termination request received");
+            postMessage({"type":"msg", "content":"confirm-termination"});
+            isTerminated = true;
+            break;
+        default:
+            console.log("[worker] unknown command received: " + e.data.cmd);
+            postMessage({"type":"msg", "content":"unknown-command: " + e.data.cmd});
+    }
+}
+timedCount();
