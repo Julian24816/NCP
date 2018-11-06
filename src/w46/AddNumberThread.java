173265cafe1490e35ae6acf64ac42ade04c9a39e
Julian24816 <Julian24816@gmail.com>
2018-11-06 17:15:19 +0100

    Set that is not thread-safe

---
+++ src/w46/AddNumberThread.java
@@ -0,0 +1,43 @@
+package w46;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class AddNumberThread extends Thread {
+    private UniqueNumberList numberList;
+
+    private AddNumberThread(UniqueNumberList list) {
+        numberList = list;
+    }
+
+    @Override
+    public void run() {
+        int i = 0;
+        while (!isInterrupted()) numberList.add(i++);
+    }
+
+    public static void main(String[] args) {
+        try {
+            UniqueNumberList numberList = new UniqueNumberList();
+
+            List<Thread> threads = new LinkedList<>();
+            for (int i = 0; i < 10; i++) {
+                threads.add(new AddNumberThread(numberList));
+            }
+            threads.forEach(Thread::start);
+
+            Thread.sleep(2000);
+
+            threads.forEach(Thread::interrupt);
+            for (Thread thread : threads) {
+                thread.join();
+            }
+
+            numberList.checkForRedundantNumber();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/w46/UniqueNumberList.java
@@ -0,0 +1,27 @@
+package w46;
+
+import java.util.Vector;
+
+class UniqueNumberList {
+    private Vector<Integer> list = new Vector<>();
+    void add(Integer i) {
+        if (!list.contains(i))
+            list.add(i);
+    }
+    void checkForRedundantNumber() {
+        boolean foundDuplicate = false;
+
+        Vector<Integer> copy = new Vector<>(list);
+
+        for (int i : list) {
+            copy.remove(i);
+            if (copy.contains(i)) {
+                foundDuplicate = true;
+                System.out.println("Found duplicated number: " + i);
+            }
+        }
+        if (!foundDuplicate) System.out.println("Didn't find any duplicate numbers.");
+    }
+
+
+}
