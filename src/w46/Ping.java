d2687db3e049c4fe6d036b2a7b9e528d67f727e5
Julian Müller <julian.mueller@stud-mail.uni-wuerzburg.de>
2018-11-13 17:46:45 +0100

    correct interrupt pattern

--- src/w46/Ping.java
+++ src/w46/Ping.java
@@ -2,21 +2,20 @@ package w46;
 
 public class Ping extends Thread {
     private final PingPongGameState ppState;
+
     Ping(PingPongGameState pps) {
         this.ppState = pps;
     }
 
     @Override
     public void run() {
-        try {
-            while (!isInterrupted()) {
-                synchronized (ppState) {
-                    while(ppState.isPING()) {
-                        ppState.wait();
-                    }
-                    ppState.setPING();
-                    ppState.notifyAll();
+        while (!isInterrupted()) try {
+            synchronized (ppState) {
+                while (ppState.isPING()) {
+                    ppState.wait();
                 }
+                ppState.setPING();
+                ppState.notifyAll();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
--- src/w46/Pong.java
+++ src/w46/Pong.java
@@ -2,21 +2,20 @@ package w46;
 
 public class Pong extends Thread {
     private final PingPongGameState ppState;
+
     Pong(PingPongGameState pps) {
         this.ppState = pps;
     }
 
     @Override
     public void run() {
-        try {
-            while (!isInterrupted()) {
-                synchronized (ppState) {
-                    while(ppState.isPONG()) {
-                        ppState.wait();
-                    }
-                    ppState.setPONG();
-                    ppState.notifyAll();
+        while (!isInterrupted()) try {
+            synchronized (ppState) {
+                while (ppState.isPONG()) {
+                    ppState.wait();
                 }
+                ppState.setPONG();
+                ppState.notifyAll();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
