cb6340ab87e64b5ce5aa4f2bd9ef4c2fd79a8790
Julian Müller <julian.mueller@stud-mail.uni-wuerzburg.de>
2018-12-04 16:35:52 +0100

    scheduled Task to kill the first sceduled Task

--- src/w49/ISGameServerDownCheck.java
+++ src/w49/ISGameServerDownCheck.java
@@ -10,16 +10,13 @@ import java.util.concurrent.TimeUnit;
 public class ISGameServerDownCheck {
     private static Random random = new Random();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
 
         ScheduledFuture<?> scheduledTask = scheduledExecutorService.scheduleAtFixedRate(
                 () -> System.out.println(random.nextInt(2) == 0 ? "game server down" : "still connected."),
                 0, 30, TimeUnit.SECONDS);
-
-        System.out.println("Press Enter to exit game");
-        System.in.read();
-        scheduledTask.cancel(true);
+        scheduledExecutorService.schedule(() -> scheduledTask.cancel(true), 10, TimeUnit.MINUTES);
         scheduledExecutorService.shutdownNow();
     }
 }
