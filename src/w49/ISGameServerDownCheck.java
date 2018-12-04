6569b487f0545358ab8f7a58eef7d50cfdb41d2b
Julian Müller <julian.mueller@stud-mail.uni-wuerzburg.de>
2018-12-04 16:54:15 +0100

    better task shutdown

--- src/w49/ISGameServerDownCheck.java
+++ src/w49/ISGameServerDownCheck.java
@@ -2,22 +2,20 @@ package w49;
 
 import java.io.IOException;
 import java.util.Random;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class ISGameServerDownCheck {
     private static Random random = new Random();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
 
         ScheduledFuture<?> scheduledTask = scheduledExecutorService.scheduleAtFixedRate(
                 () -> System.out.println(random.nextBoolean() ? "game server down" : "still connected."),
-                0, 30, TimeUnit.SECONDS);
-        scheduledExecutorService.schedule(() -> scheduledTask.cancel(true), 10, TimeUnit.MINUTES);
+                0, 1, TimeUnit.SECONDS);
+        scheduledExecutorService.schedule(() -> scheduledTask.cancel(true), 10, TimeUnit.SECONDS);
 
+        try {scheduledTask.get();} catch (CancellationException e) {}
         scheduledExecutorService.shutdownNow();
     }
 }
