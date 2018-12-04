acde7f963d8b13447cbe0b9c76def9bd1956a52e
Julian Müller <julian.mueller@stud-mail.uni-wuerzburg.de>
2018-12-04 16:02:28 +0100

    task 2

---
+++ src/w49/BlockingQueueRandomNumberProducerConsumerStuffExecutor.java
@@ -0,0 +1,29 @@
+package w49;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class BlockingQueueRandomNumberProducerConsumerStuffExecutor {
+    public static void main(String[] args) {
+        BlockingQueue<Integer> list = new LinkedBlockingQueue<>();
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(() -> {
+            Random random = new Random();
+            while (!Thread.currentThread().isInterrupted()) try {
+                list.put(random.nextInt(1000));
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        executorService.submit(() -> {
+            while (!Thread.currentThread().isInterrupted()) try {
+                System.out.println(list.take());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        try {Thread.sleep(1000);} catch (InterruptedException e) { System.out.println("sleep interrupted"); }
+        executorService.shutdownNow();
+        try { executorService.awaitTermination(1000, TimeUnit.MILLISECONDS); } catch (InterruptedException e) { e.printStackTrace(); }
+    }
+}
