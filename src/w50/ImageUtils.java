a68ec2cc365bf64927f8fb2e5cd1b1fd4404ec03
Julian Müller <julian.mueller@stud-mail.uni-wuerzburg.de>
2018-12-11 17:09:17 +0100

    use recursive task and fork join pattern

--- src/w50/ImageUtils.java
+++ src/w50/ImageUtils.java
@@ -6,26 +6,50 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Random;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
 
 
 public class ImageUtils {
 
-    // test
-    public static void main (String [] args){
-        ImageUtils.example(600,300);
-    }
+    private static class RecursiveGeneration extends RecursiveTask<BufferedImage> {
 
-    private static void example(int width, int height) {
-        try {
-            BufferedImage image1 = makeRandomImage(width, height);
-            BufferedImage image2 = makeRandomImage(width, height);
-            BufferedImage image3 = joinBufferedImage (image1, image2,0);
-            save(image3,"./output2.bmp");
-        } catch (IOException e) {
-            e.printStackTrace();
+        private int width, height;
+
+        private RecursiveGeneration(int width, int height) {
+            this.width = width;
+            this.height = height;
         }
+
+        @Override
+        protected BufferedImage compute() {
+            if (shouldSplitTheWork(width))
+                return splitTheWork(width, height);
+            return doTheWork(width, height);
+        }
+
+        private BufferedImage splitTheWork(int width, int height) {
+            int splitWidth = Math.min(WIDTH_THRESHOLD, width / 2);
+            ForkJoinTask<BufferedImage> task1 = new RecursiveGeneration(splitWidth, height).fork(),
+                    task2 = new RecursiveGeneration(width - splitWidth, height).fork();
+            task1.fork();
+            return joinBufferedImage(task1.join(), task2.join(), 0);
+        }
+
+        private boolean shouldSplitTheWork(int width) {
+            return width > WIDTH_THRESHOLD;
+        }
+
     }
 
+    private static final int WIDTH_THRESHOLD = 100;
+
+    // test
+    public static void main (String [] args) throws IOException {
+        save(new RecursiveGeneration(1200, 300).fork().join(), "./" + "output" + ".bmp");
+    }
+
+    @SuppressWarnings("SameParameterValue")
     private static void save(BufferedImage image, String path) {
         try {
             File outputFile = new File(path);
@@ -35,32 +59,42 @@ public class ImageUtils {
         }
     }
 
-    private static BufferedImage makeRandomImage(int width, int height) {
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    private static BufferedImage doTheWork(int width, int height) {
         int[][] red = new int[height][width];
         int[][] green = new int[height][width];
         int[][] blue = new int[height][width];
 
-        Random random = new Random();
-        for (int h = 0; h < height; h++) {
-            for (int w = 0; w < width; w++) {
-                red[h][w] = random.nextInt(255); // here just playing with Red channel
-                green[h][w] = random.nextInt(255); // here just playing with Red channel
-                blue[h][w] = random.nextInt(255); // here just playing with Red channel
-            }
-        }
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int rgb = red[y][x];
-                rgb = (rgb << 8) + green[y][x];
-                rgb = (rgb << 8) + blue[y][x];
-                image.setRGB(x, y, rgb);
-            }
-        }
+        fillRGBArraysWithRandomValues(width, height, red, green, blue);
+        return buildBufferedImageFromRGBArrays(width, height, red, green, blue);
+    }
+
+    private static BufferedImage buildBufferedImageFromRGBArrays(int width, int height, int[][] red, int[][] green, int[][] blue) {
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        for (int y = 0; y < height; y++)
+            for (int x = 0; x < width; x++)
+                image.setRGB(x, y, computeRGBValue(red[y][x], green[y][x], blue[y][x]));
         return image;
     }
 
-    private static BufferedImage joinBufferedImage(BufferedImage img1, BufferedImage img2, int offset) throws IOException {
+    private static int computeRGBValue(int red, int green, int blue) {
+        int rgb = red;
+        rgb = (rgb << 8) + green;
+        rgb = (rgb << 8) + blue;
+        return rgb;
+    }
+
+    private static void fillRGBArraysWithRandomValues(int width, int height, int[][] red, int[][] green, int[][] blue) {
+        Random random = new Random();
+        for (int h = 0; h < height; h++) {
+            for (int w = 0; w < width; w++) {
+                red[h][w] = random.nextInt(255);
+                green[h][w] = random.nextInt(255);
+                blue[h][w] = random.nextInt(255);
+            }
+        }
+    }
+
+    private static BufferedImage joinBufferedImage(BufferedImage img1, BufferedImage img2, int offset) {
 
         int width = img1.getWidth()+img2.getWidth()+offset;
         int height = Math.max(img1.getHeight(),img2.getHeight()); //+offset;
